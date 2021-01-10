6527e4b33bdd4325790600005082b4d51b6abb7a
IvanFG <ifg1202@gmail.com>
2021-01-10 19:30:29 +0100

    Degrade twice and quality never negative

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,8 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    private static final int DEGRADE_RANGE = 2;
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -11,11 +13,7 @@ class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                degradeQuality(items[i]);
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -43,11 +41,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                        degradeQuality(items[i]);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
@@ -56,6 +50,19 @@ class GildedRose {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
+            }
+        }
+    }
+
+    private void degradeQuality(Item item) {
+        if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if(item.quality == 1) {
+                    item.quality = 0;
+                } else {
+                    item.quality = item.quality - DEGRADE_RANGE;
+                }
+
             }
         }
     }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,49 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @DisplayName("Once the sell by date has passed, Quality degrades twice as fast")
+    void qualityDegrades() {
+        //given
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 7) };
         GildedRose app = new GildedRose(items);
+        // when
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        // then
+        assertEquals(5, app.items[0].quality);
     }
+
+    @Test
+    @DisplayName("Given quality 1, final quality must be 0")
+    void qualityOneDegrades() {
+        //given
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 1) };
+        GildedRose app = new GildedRose(items);
+        // when
+        app.updateQuality();
+        // then
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    @DisplayName("The Quality of an item is never negative")
+    void qualityNeverNegative() {
+        //given
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        // when
+        app.updateQuality();
+        // then
+        assertTrue(app.items[0].quality >= 0);
+    }
+
 
 }
